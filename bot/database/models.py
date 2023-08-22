6971ae049cf97b0c4ee8a200b1be844a5438e680
Igor Vasiljev <93223722+va1ngvarr@users.noreply.github.com>
2023-08-22 22:20:01 +0500

    Update models.py

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,11 +1,6 @@
 from peewee import (
     Model,
-    IntegerField,
     CharField,
-    AutoField,
-    ForeignKeyField,
-    DateTimeField,
-    BooleanField,
     SqliteDatabase,
 )
 
@@ -15,12 +10,6 @@ conn = SqliteDatabase("sqlite3.db")
 class BaseModel(Model):
     class Meta:
         database = conn
-
-
-class Profile(BaseModel):
-    user_id = IntegerField(unique=True)
-    username = CharField(max_length=255, null=True)
-    chat_id = IntegerField(unique=True)
 
 
 class Channels(BaseModel):
